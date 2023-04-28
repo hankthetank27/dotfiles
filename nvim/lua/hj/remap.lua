e503f3883ee0a23eaa353aa45ebf0e7946df618b
Hank Jackson <hjackson277@gmail.com>
2023-04-28 14:54:09 -0400

    remaps

--- nvim/lua/hj/remap.lua
+++ nvim/lua/hj/remap.lua
@@ -1,4 +1,29 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
+
+-- moves highlighted text
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- cursor stays in place when appending to line above
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- centers half screen jump
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- centers search terms
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- pastes buffer without storing highlighted text to buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- edits all instances of word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- etc
+vim.keymap.set("n", "<C-/>", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>pv",  vim.cmd.Ex)
+vim.keymap.set("n", "Q", "<nop>")
--- nvim/lua/hj/set.lua
+++ nvim/lua/hj/set.lua
@@ -7,14 +7,9 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.undodir = os.getenv('HOME') .. '/.vim/undodir'
-vim.opt.undofile = true
-
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.updatetime = 50
@@ -22,3 +17,8 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = '80'
 
 vim.opt.signcolumn = 'yes'
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
