40d648d390efeb393f6dcc15ac0069c69d40572c
Hank Jackson <hjackson277@gmail.com>
2023-05-02 11:49:05 -0400

    packer bootstrap

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -2,7 +2,7 @@ vim.o.termguicolors = true
 vim.api.nvim_command([[
     augroup ModifyColors
         autocmd colorscheme * :hi normal guibg=#211f1d
-        hi DiagnosticError guifg=Red 
+        hi DiagnosticError guifg=Red
         hi DiagnosticWarn  guifg=DarkOrange
         hi DiagnosticInfo  guifg=Blue
         hi DiagnosticHint  guifg=Green
