5fa6b26d36dd736a8f3516767af41a468ce7b84b
Hank Jackson <hjackson277@gmail.com>
2023-05-03 21:34:39 -0400

    lsp hover remap

--- nvim/lua/hj/lsp.lua
+++ nvim/lua/hj/lsp.lua
@@ -33,7 +33,7 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>ee", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
@@ -47,6 +47,3 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
-
--- diagnostic hover window
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
