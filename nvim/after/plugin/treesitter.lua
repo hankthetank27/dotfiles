2042c81bfb54cc8186d13de89a0ea5452ea34131
Hank Jackson <hjackson277@gmail.com>
2023-05-17 18:34:40 -0400

    treesitter indent enabled

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,25 +1,25 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-     ensure_installed = {
-         "javascript",
-         "typescript",
-         "rust",
-         "python",
-         "scheme",
-         "c",
-         "lua",
-         "vim",
-         "vimdoc",
-         "query",
-         "bash"
-     },
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "rust",
+        "python",
+        "scheme",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "bash"
+    },
 
-     sync_install = false,
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-     auto_install = true,
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-     highlight = {
+    highlight = {
         enable = true,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -27,5 +27,9 @@ require'nvim-treesitter.configs'.setup {
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
-     },
+    },
+
+    indent = {
+        enable = true
+    }
 }
