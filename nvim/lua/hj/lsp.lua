fb3d7b57dc277cc944d98ed859676a20a4664959
Hank Jackson <hjackson277@gmail.com>
2023-10-27 11:54:26 -0400

    go lsp

--- nvim/lua/hj/lsp.lua
+++ nvim/lua/hj/lsp.lua
@@ -1,21 +1,18 @@
-local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
+local lsp = require("lsp-zero").preset("recommended")
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-Enter>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete()
+lsp.setup_nvim_cmp({
+    mapping = lsp.defaults.cmp_mappings({
+        ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
 })
-
-lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
