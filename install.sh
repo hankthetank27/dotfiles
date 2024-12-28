a8b22cd97dfdc6b95cbb2ffa5503487958f510f7
Hank Jackson <hjackson277@gmail.com>
2024-12-28 14:28:56 -0500

    update install script

--- install.sh
+++ install.sh
@@ -2,127 +2,142 @@
 
 # Create symlinks for dotfiles
 
-OS=$(uname)
 RED='\033[0;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[0;35m'
 NO_COLOR='\033[0m'
 
+OS=$(uname)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+FONT_DIR="$SCRIPT_DIR/assets/fonts"
+
 get_name() {
-  local filepath="$1"
-  filepath="${filepath%/}" # Remove trailing slash if present
-  echo "${filepath##*/}" # Extract the file name
+    local filepath="$1"
+    filepath="${filepath%/}" # Remove trailing slash if present
+    echo "${filepath##*/}" # Extract the file name
 }
 
 file_exists() {
-  [ -e "$1" ] || [ -L "$1" ] # Check if file or symlink exists
+    [ -e "$1" ] || [ -L "$1" ] # Check if file or symlink exists
 }
 
 link_paths() {
-  local src="$1"
-  local dest="$2"
-  ln -sfn "$src" "$dest"
-  echo -e "${GREEN}Linked paths ${NO_COLOR}$src ${GREEN}---> ${NO_COLOR}$dest"
+    local src="$1"
+    local dest="$2"
+    ln -sfn "$src" "$dest"
+    installed_message $src $dest
+}
+
+installed_message() {
+    local src="$1"
+    local dest="$2"
+    echo -e "${GREEN}Installed ${NO_COLOR}$src ${GREEN}---> ${NO_COLOR}$dest"
 }
 
 file_exists_message() {
-  local path="$1"
-  local path_type="$2"
-  echo -e "${YELLOW}$path_type already exists at destination${NO_COLOR} $dest"
+    local src="$1"
+    echo -e "${YELLOW}Skipping ${NO_COLOR}$src${YELLOW} - already exists"
 }
 
-make_link() {
-  local src="$1"
-  local dest="$2"
-  local force="$3"
-  
-  if [ -d "$src" ]; then
-    local name=$(get_name "$src")
-    local dest_dir="${dest%/}/${name}" # Ensure no trailing slash on dest, append name
-    
-    if ! file_exists "$dest_dir" || [ "$force" == "force" ]; then
-      link_paths "$src" "$dest_dir"
+install_symlink() {
+    local src="$1"
+    local dest="$2"
+    local overwrite="$3"
+
+    if [ -d "$src" ]; then
+        local name=$(get_name "$src")
+        local dest_dir="${dest%/}/${name}" # Ensure no trailing slash on dest, append name
+
+        if ! file_exists "$dest_dir" || [ "$overwrite" == "overwrite" ]; then
+            link_paths "$src" "$dest_dir"
+        else
+            file_exists_message "$src" "$des_dir"
+        fi
+    elif [ -f "$src" ]; then
+        if ! file_exists "$dest" || [ "$overwrite" == "overwrite" ]; then
+            link_paths "$src" "$dest"
+        else
+            file_exists_message "$src" "$dest"
+        fi
     else
-      file_exists_message "$dest_dir" "Directory"
+        echo -e "${RED}$src is not a valid file or directory"
     fi
-  elif [ -f "$src" ]; then
-    if ! file_exists "$dest" || [ "$force" == "force" ]; then
-      link_paths "$src" "$dest"
-    else
-      file_exists_message "$dest_dir" "File"
-    fi
-  else
-    echo -e "${RED}$src is not a valid file or directory"
-  fi
 }
 
-# Check for "force" option
-force_flag=""
-if [[ "$1" == "force" ]]; then
-  force_flag="force"
-fi
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-# editor
-make_link "$SCRIPT_DIR/editor/nvim/" "$HOME/.config/" "$force_flag"
-make_link "$SCRIPT_DIR/editor/.vimrc" "$HOME/.vimrc" "$force_flag"
-make_link "$SCRIPT_DIR/editor/zed/keymap.json" "$HOME/.config/zed/keymap.json" "$force_flag"
-make_link "$SCRIPT_DIR/editor/zed/settings.json" "$HOME/.config/zed/settings.json" "$force_flag"
-
-# home/shell
-make_link "$SCRIPT_DIR/home/.tmux.conf" "$HOME/.tmux.conf" "$force_flag"
-make_link "$SCRIPT_DIR/home/.bashrc" "$HOME/.bashrc" "$force_flag"
-make_link "$SCRIPT_DIR/home/.inputrc" "$HOME/.inputrc" "$force_flag"
-make_link "$SCRIPT_DIR/home/.bash_aliases" "$HOME/.bash_aliases" "$force_flag"
-make_link "$SCRIPT_DIR/home/.gitconfig" "$HOME/.gitconfig" "$force_flag"
-make_link "$SCRIPT_DIR/home/kitty/" "$HOME/.config/" "$force_flag"
-if [ "$OS" = "Darwin" ]; then
-    make_link "$SCRIPT_DIR/home/yabai/" "$HOME/.config/" "$force_flag"
-    make_link "$SCRIPT_DIR/home/skhd/" "$HOME/.config/" "$force_flag"
-fi
-
-# bin
-make_link "$SCRIPT_DIR/bin/tde.sh" "$HOME/.local/bin/tde" "$force_flag"
-
-# assets
-FONT_DIR="$SCRIPT_DIR/assets/fonts"
 install_font() {
     local src="$1"
     local dest="$2"
+    local overwrite="$3"
     local filename=$(basename "$src")
-    if [ -f "$dest/$filename" ]; then
-        echo "Skipping $filename - already exists"
+    if [ -f "$dest/$filename" ] && [ "$overwrite" != "overwrite" ]; then
+        file_exists_message "$filename"
     else
         cp "$src" "$dest/"
-        echo "Installed $filename"
+        installed_message "$src" "$dest"
     fi
 }
 
 install_fonts_mac() {
+    local overwrite="$1"
     local dest_dir="$HOME/Library/Fonts"
     mkdir -p "$dest_dir"
     for font in "$FONT_DIR"/*; do
-        install_font "$font" "$dest_dir"
+        install_font "$font" "$dest_dir" "$overwrite"
     done
 }
 
 install_fonts_linux() {
+    local overwrite="$1"
     local dest_dir="$HOME/.local/share/fonts"
     mkdir -p "$dest_dir"
     for font in "$FONT_DIR"/*; do
-        install_font "$font" "$dest_dir"
+        install_font "$font" "$dest_dir" "$overwrite"
     done
     fc-cache -f -v
 }
 
-if [ "$OS" = "Darwin" ]; then
-    install_fonts_mac
-elif [ "$OS" = "Linux" ]; then
-    install_fonts_linux
-else
-    echo "Unsupported operating system"
+
+# Check for "overwrite" option
+overwrite_flag=""
+if [[ "$1" == "--overwrite" || "$1" == "-o" ]]; then
+    read -p "Are you sure you want to overwrite your existing configurations? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        overwrite_flag="overwrite";
+    else
+        exit 0;
+    fi
 fi
 
-echo -e "${GREEN}Installation complete"
+# editor
+install_symlink "$SCRIPT_DIR/editor/nvim/" "$HOME/.config/" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/editor/.vimrc" "$HOME/.vimrc" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/editor/zed/keymap.json" "$HOME/.config/zed/keymap.json" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/editor/zed/settings.json" "$HOME/.config/zed/settings.json" "$overwrite_flag"
+
+# home/shell
+install_symlink "$SCRIPT_DIR/home/.tmux.conf" "$HOME/.tmux.conf" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/home/.bashrc" "$HOME/.bashrc" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/home/.inputrc" "$HOME/.inputrc" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/home/.bash_aliases" "$HOME/.bash_aliases" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/home/.gitconfig" "$HOME/.gitconfig" "$overwrite_flag"
+install_symlink "$SCRIPT_DIR/home/kitty/" "$HOME/.config/" "$overwrite_flag"
+if [ "$OS" = "Darwin" ]; then
+    install_symlink "$SCRIPT_DIR/home/yabai/" "$HOME/.config/" "$overwrite_flag"
+    install_symlink "$SCRIPT_DIR/home/skhd/" "$HOME/.config/" "$overwrite_flag"
+fi
+
+# bin
+install_symlink "$SCRIPT_DIR/bin/tde.sh" "$HOME/.local/bin/tde" "$overwrite_flag"
+
+# assets
+if [ "$OS" = "Darwin" ]; then
+    install_fonts_mac "$overwrite_flag"
+elif [ "$OS" = "Linux" ]; then
+    install_fonts_linux "$overwrite_flag"
+else
+    echo "${RED}Could not install fonts. Unsupported operating system."
+fi
+
+echo -e "${GREEN}Installation complete."
 
